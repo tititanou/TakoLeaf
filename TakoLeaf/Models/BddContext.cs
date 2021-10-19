cb99a5f1fad84fd7196ec037d08a53e735145c73
Abrate Alexis <abrate.alexis@outlook.fr>
2021-10-19 17:19:37 +0200

    Changement vraiment du nom BDD

---
+++ TakoLeaf/Models/BddContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakoLeaf.Models
+{
+    public class BddContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user id=root;password=mysql;database=TakoLeaf");
+        }
+    }
+}
