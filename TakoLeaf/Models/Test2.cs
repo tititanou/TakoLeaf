695c1bda6c6ae30d6633a8194f55c4ed7592cc42
Abrate Alexis <abrate.alexis@outlook.fr>
2021-10-19 16:39:12 +0200

    Ajout test 2

---
+++ TakoLeaf/Models/Test2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakoLeaf.Models
+{
+    public class Test2
+    {
+        public string test2 { get; set; }
+    }
+}
