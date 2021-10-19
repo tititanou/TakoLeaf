cf2458db3cca1a0fe916bccc2cdd9e2dfe1c8b9e
Abrate Alexis <abrate.alexis@outlook.fr>
2021-10-19 16:24:34 +0200

    Debut Projet
    Squelette du projet et architecture

---
+++ TakoLeaf/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakoLeaf.Models
+{
+    public class Class
+    {
+
+        public int nombre { get; set; }
+    }
+}
