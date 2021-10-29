eb40ae4b9f97d3014675697061cf46f877645808
tititanou <tania.rojas@edu.itescia.fr>
2021-10-29 12:45:54 +0200

    Début messagerie

--- TakoLeaf/Controllers/MessagerieController.cs
+++ TakoLeaf/Controllers/MessagerieController.cs
@@ -1,21 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TakoLeaf.Data;
+using TakoLeaf.Models;
 
 namespace TakoLeaf.Controllers
 {
     [Authorize]
     public class MessagerieController : Controller
     {
-        //private IDalMessagerie dalForum;
+        private IDalMessagerie dalMessagerie;
         private int userId;
+
         public MessagerieController()
         {
-            //this.dalForum = new DalMessagerie();
+            this.dalMessagerie = new DalMessagerie();
 
+        }
+
+        public ActionResult MessagesEnvoyes()
+        {
+            userId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            List<Message> messages = this.dalMessagerie.GetMessageEnvoyes(userId).ToList();
+            return View(messages);
+        }
+
+        public ActionResult MessagesRecus()
+        {
+            userId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            List<Message> messages = this.dalMessagerie.GetMessageRecus(userId).ToList();
+            return View(messages);
         }
     }
 }
---
+++ TakoLeaf/Data/DalMessagerie.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TakoLeaf.Models;
+
+namespace TakoLeaf.Data
+{
+    public class DalMessagerie : IDalMessagerie
+    {
+        private BddContext _bddContext;
+
+        public DalMessagerie()
+        {
+            this._bddContext = new BddContext();
+        }
+
+        public void Dispose()
+        {
+            this._bddContext.Dispose();
+        }
+
+        public void CreationMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetMessageRecus(int id)
+        {
+            List<Message> allMessages = this._bddContext.Messages.Include(m => m.AdherentDest).Include(m => m.AdherentExp).ToList();
+            List<Message> messageRecus = new List<Message>();
+            foreach(Message message in allMessages)
+            {
+                if (message.AdherentDestId == id)
+                {
+                    messageRecus.Add(message);
+                }
+            }
+            return messageRecus;
+        }
+
+        public List<Message> GetMessageEnvoyes(int id)
+        {
+            List<Message> allMessages = this._bddContext.Messages.Include(m => m.AdherentDest).Include(m => m.AdherentExp).ToList();
+            List<Message> messageEnvoyes = new List<Message>();
+            foreach (Message message in allMessages)
+            {
+                if (message.AdherentExpId == id)
+                {
+                    messageEnvoyes.Add(message);
+                }
+            }
+            return messageEnvoyes;
+        }
+
+        public Message GetMessage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SuppressionMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
