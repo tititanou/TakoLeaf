d7e99a94ebd9c00a9cd56421077a1568fa7ba8d0
tititanou <tania.rojas@edu.itescia.fr>
2021-10-29 13:54:09 +0200

    Table intermédiaire messagerie

---
+++ TakoLeaf/Models/Messagerie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TakoLeaf.Models
+{
+    public class Messagerie
+    {
+        
+        public int IdMessage { get; set; }
+        public Message Message { get; set; }
+
+        public int IdExpediteur { get; set; }
+        public Adherent Expediteur { get; set; }
+
+        public int IdDestinataire { get; set; }
+        public Adherent Destinataire { get; set; }
+        
+    }
+}
